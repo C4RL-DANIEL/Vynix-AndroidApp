8feef6cf4f081f92bddadace031e723e6b561e55
C4RL-DANIEL <aglibotcarldaniel@gmail.com>
2026-08-03 00:32:28 +0800

    Fix Compose version mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,6 +112,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.01.00"))
+    androidTestImplementation(enforcedPlatform("androidx.compose:compose-bom:2024.01.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
